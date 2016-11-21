a31f410c9b44bb2eba1af4e6f3345080897fa396
HanqiangSong <hanqiangsong1024@gmail.com>
2016-11-20 23:34:42 -0800

    week2

---
+++ example.js
@@ -0,0 +1,21 @@
+var http = require("http") ;
+var fs = require("fs");
+//var server = http.createServer();
+// server.listen(3000);
+
+http.createServer(function (req, res) {
+    if (req.url === "/") {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        var obj = {
+            name: "Song",
+            age: 23
+        };
+        res.end(JSON.stringify(obj));
+    }
+        if (req.url === "/api") {
+            res.writeHead(200, {"Content-Type": "application"});
+            var html = fs.readFileSync(__dirname + "/index.html");
+            res.end(html);
+        }
+
+}).listen(3000);
